3b615507d52984c892c486475822b87daea691e9
Bauyrzhan <billfookingmurray@gmail.com>
2026-02-13 10:34:24 +0500

    feat: teachers lessons and schedule api

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from app.core.database import Base
 from .attendance_record import AttendanceRecord
 from .platonus_employee import PlatonusEmployee
 from .work_schedule_rule import WorkScheduleRule
+from .teacher_schedule import TeacherSchedule
 
-__all__ = ["Base", "AttendanceRecord", "PlatonusEmployee", "WorkScheduleRule"]
+__all__ = ["Base", "AttendanceRecord", "PlatonusEmployee", "WorkScheduleRule", "TeacherSchedule"]
